ff12ca0452af3debb6b57daa9b99e72c6c98a17e
Matthias.desktp <moritz370@googlemail.com>
2015-09-07 13:05:35 +0200

    Adding Test Cases and do some cosmetics

--- src/Controller/Component/IsbnComponent.php
+++ src/Controller/Component/IsbnComponent.php
@@ -21,42 +21,53 @@ class IsbnComponent extends Component
 
     private function getGroupNumber($isbn){
         $lengr = 0;
-        if (preg_match("/978[0-5^7][0-9]{9}/", $isbn)){
+        if (preg_match("/978[0-5^7][0-9]{9}/", $isbn))
+        {
             $lengr = 1;
         }
-        if (preg_match("/978([8][0-9]|[9][0-4])[0-9]{8}/", $isbn)){
+        if (preg_match("/978([8][0-9]|[9][0-4])[0-9]{8}/", $isbn))
+        {
             $lengr = 2;
         }
-        if (preg_match("/97910[0-9]{8}/", $isbn)){
+        if (preg_match("/97910[0-9]{8}/", $isbn))
+        {
             $lengr = 2;
         }
-        if (preg_match("/978(6[0-4][0-9]|9[5-8][0-9])[0-9]{7}/", $isbn)){
+        if (preg_match("/978(6[0-4][0-9]|9[5-8][0-9])[0-9]{7}/", $isbn))
+        {
             $lengr = 3;
         }
-        if (preg_match("/97899[0-8][0-9][0-9]{6}/", $isbn)){
+        if (preg_match("/97899[0-8][0-9][0-9]{6}/", $isbn))
+        {
             $lengr = 4;
         }
-        if (preg_match("/978999[0-9][0-9][0-9]{5}/", $isbn)){
+        if (preg_match("/978999[0-9][0-9][0-9]{5}/", $isbn))
+        {
             $lengr = 5;
         }        
         return (substr($isbn, 3, $lengr));
     }
     
-    private function getPublisherNumber($isbn){
+    private function getPublisherNumber($isbn)
+    {
         
    
         $groupnumber = $this->getGroupNumber($isbn);
         $number = substr($isbn, 3+strlen($groupnumber), -2)+0;
         $xml = simplexml_load_file(Plugin::path('Isbn').'webroot'. DS .'files'. DS .'RangeMessage.xml');
-        foreach ($xml->RegistrationGroups->Group as $i){
-            if ('978-'.$groupnumber == $i->Prefix){
-                foreach ($i->Rules->Rule as $j){
+        foreach ($xml->RegistrationGroups->Group as $i)
+        {
+            if ('978-'.$groupnumber == $i->Prefix)
+            {
+                foreach ($i->Rules->Rule as $j)
+                {
                     $limits = (explode("-",$j->Range));
                     $lengpub = $j->Length[0]+0;
                     $minimum = $limits[0]+0;
                     $maximum = $limits[1]+0;
 
-                    if ($number >= $minimum and $number <= $maximum){
+                    if ($number >= $minimum and $number <= $maximum)
+                    {
                         $rv = (substr($isbn, 3+strlen($groupnumber), $lengpub));
                         return $rv;
                     }
@@ -74,7 +85,12 @@ class IsbnComponent extends Component
 * @access public
 * @throws NO ERROR HANDLING
 * */
-    public function splitIsbn($isbn){
+    public function splitIsbn($isbn)
+    {
+        if (!$this->validateIsbn($isbn))
+        {
+            return false;
+        } 
         $prefix         = substr($isbn, 0,3);
         $groupnumber    = $this->getGroupNumber($isbn);
         $pubnumber      = $this->getPublisherNumber($isbn);
@@ -82,6 +98,38 @@ class IsbnComponent extends Component
         $articleNumber  = substr($isbn, $prenumbers, -1);
         $checkdigit     = substr($isbn, -1);
         return (array($prefix, $groupnumber, $pubnumber, $articleNumber, $checkdigit));        
+    }
+
+/**
+* Validates an ISBN-13 Number
+*
+* @param string $input The ISBN Numeber.
+* @returnbool
+* */    
+    public function validateIsbn($check)
+    {
+        $ean = strval($check);
+        $code = substr($ean, 0, -1);
+        $key = 0;
+        $mult = array( 1, 3 );           
+        for ( $i = 0; $i < strlen( $code ); $i++ )
+        {            
+            $key += substr( $code, $i, 1 ) * $mult[$i % 2];
+        }
+        $key = 10 - ( $key % 10 );
+        if ( $key == 10 )
+        {
+            $key = 0;
+        }
+        // in key steht die prüfziffer - an den code anhängen
+        $code .= $key;
+        if ($code == $ean)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
     }
 
 }
--- tests/TestCase/Controller/Component/IsbnComponentTest.php
+++ tests/TestCase/Controller/Component/IsbnComponentTest.php
@@ -42,13 +42,23 @@ class IsbnComponentTest extends TestCase
      */
     public function testSplitIsbn()
     {
-        $result = $this->IsbnComponent->splitIsbn('9783957570000');
-        $expected = array ("978", "3", "95757","000","0");
-        $this->assertEquals($expected , $result);
-        
-        $result = $this->IsbnComponent->splitIsbn('9783789158605');
-        $expected = array ("978", "3", "7891","5860","5");
-        $this->assertEquals($expected , $result);
-        
+        $cases = array
+        (
+            '9783957570000' => array ("978", "3", "95757", "000", "0"),
+            '9783789158605' => array ("978", "3", "7891", "5860", "5"),
+            '9783789158604' => false,
+            '9783832197575' => array ("978", "3", "8321", "9757", "5"),
+            '9780747411727' => array ("978", "0", "7474", "1172", "7"),
+            '9783829605984' => array ("978", "3", "8296", "0598", "4"),
+            '9783000298080' => array ("978", "3", "00", "029808", "0"),
+            '9788891506511' => array ("978", "88", "91", "50651", "1")
+
+        );
+        foreach ($cases as $isbn => $split)
+        {
+            $result = $this->IsbnComponent->splitIsbn($isbn);
+            $expected = $split;
+            $this->assertEquals($expected , $result);
+        }
     }
 }
